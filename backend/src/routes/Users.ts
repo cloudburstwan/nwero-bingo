70fb89649f69eb65dcd3eda0b9b1ba0d2330f729
Cloudburst <cloudburst@cloudburst.lgbt>
2026-07-10 15:50:39 +0100

    Added API endpoint for getting user by ID

--- backend/src/routes/Users.ts
+++ backend/src/routes/Users.ts
@@ -2,6 +2,7 @@ import express from "express";
 import Database from "../database";
 import Sessions from "../sessions";
 import requireSessionMiddleware, { RequestWithSession } from "../utils/RequireSessionMiddleware";
+import APIError from "../types/APIError";
 
 export function UsersAPI(database: Database, sessions: Sessions) {
   const api = express.Router();
@@ -16,7 +17,21 @@ export function UsersAPI(database: Database, sessions: Sessions) {
     });
   });
 
-  // TODO: Get user by ID (requires authentication)
+  api.get("/:id", requireSessionMiddleware(sessions), async (req: RequestWithSession, res) => {
+    try {
+      let user = await database.getUser(req.params.id as string);
+
+      res.status(200).json({
+        id: user.id,
+        username: user.username,
+        avatarUrl: user.avatarUrl
+      });
+    } catch (err) {
+      if (err instanceof ReferenceError)
+        throw new APIError(404, "ENTITY_NOT_FOUND", `Could not find a user with the id ${req.params.id}.`);
+      else throw err;
+    }
+  })
 
   return api;
 }
