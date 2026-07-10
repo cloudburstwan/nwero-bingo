0e3bc57f54562d16e8616b731b4a72c9342e83e9
Cloudburst <cloudburst@cloudburst.lgbt>
2026-07-10 13:57:29 +0100

    Fixed use of type as value

--- backend/src/database/types/templates/TemplateBucket.ts
+++ backend/src/database/types/templates/TemplateBucket.ts
@@ -28,7 +28,7 @@ export default class TemplateBucket {
 
     let output = [];
     for (const row of result.rows) {
-      output.push(new Prompt(row.id, row.bucket_id, row.prompt, row.description));
+      output.push({ id: row.id, bucketId: row.bucket_id, prompt: row.prompt, description: row.description });
     }
     return output;
   }
