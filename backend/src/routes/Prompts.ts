ed8d386f9e7a9eada9f3f4970aea2bb19ce645bc
Cloudburst <cloudburst@cloudburst.lgbt>
2026-07-10 14:15:50 +0100

    Added API endpoint for updating prompts

--- backend/src/routes/Prompts.ts
+++ backend/src/routes/Prompts.ts
@@ -4,6 +4,7 @@ import Sessions from "../sessions";
 import requireSessionMiddleware from "../utils/RequireSessionMiddleware";
 import APIError from "../types/APIError";
 import { batchCheckType } from "../utils/checkType";
+import Prompt from "../database/types/Prompt";
 
 export function PromptsAPI(database: Database, sessions: Sessions) {
   const api = express.Router();
@@ -46,7 +47,33 @@ export function PromptsAPI(database: Database, sessions: Sessions) {
     }
   })
 
-  // TODO: Update prompt (requires authentication)
+  api.patch("/:id", express.json(), requireSessionMiddleware(sessions), async (req, res) => {
+    let { checkType, completeBatch } = batchCheckType();
+    checkType("prompt", req.body.prompt, "string", true);
+    checkType("description", req.body.description, "string", true);
+    completeBatch();
+
+    try {
+      let prompt = await database.getPrompt(req.params.id as string);
+      let updatedRawData: Prompt = Object.assign(prompt, req.body);
+
+      prompt.prompt = updatedRawData.prompt;
+      prompt.description = updatedRawData.description;
+
+      await database.updatePrompt(prompt);
+
+      res.status(200).json({
+        id: prompt.id,
+        bucketId: prompt.bucketId,
+        prompt: prompt.prompt,
+        description: prompt.description,
+      });
+    } catch (err) {
+      if (err instanceof ReferenceError)
+        throw new APIError(404, "ENTITY_NOT_FOUND", `Could not find a prompt with the id ${req.params.id}.`);
+      else throw err;
+    }
+  })
 
   // TODO: Delete prompt (requires authentication)
 
