4c4544b2a69569ced27dc341602dd1e9cb79a9e1
Cloudburst <cloudburst@cloudburst.lgbt>
2026-07-10 15:48:46 +0100

    Added API endpoint for getting current user

--- backend/src/routes/Users.ts
+++ backend/src/routes/Users.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import Database from "../database";
 import Sessions from "../sessions";
+import requireSessionMiddleware, { RequestWithSession } from "../utils/RequireSessionMiddleware";
 
 export function UsersAPI(database: Database, sessions: Sessions) {
   const api = express.Router();
 
-  // TODO: Get current user (requires authentication)
+  api.get("/@me", requireSessionMiddleware(sessions), async (req: RequestWithSession, res) => {
+    let user = await database.getUser(req.session!.userId);
+
+    res.status(200).json({
+      id: user.id,
+      username: user.username,
+      avatarUrl: user.avatarUrl
+    });
+  });
 
   // TODO: Get user by ID (requires authentication)
 
