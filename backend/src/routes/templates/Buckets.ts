e0c7163b60e6e91148260fd1fad561ee61ba3193
Cloudburst <cloudburst@cloudburst.lgbt>
2026-07-11 10:43:50 +0100

    Added API endpoint for creating bucket templates

--- backend/src/routes/templates/Buckets.ts
+++ backend/src/routes/templates/Buckets.ts
@@ -4,6 +4,7 @@ import Sessions from "../../sessions";
 import APIError from "../../types/APIError";
 import requireSessionMiddleware, { RequestWithSession } from "../../utils/RequireSessionMiddleware";
 import { batchCheckType } from "../../utils/checkType";
+import TemplateBucket from "../../database/types/templates/TemplateBucket";
 
 export function TemplatesBucketsAPI(database: Database, sessions: Sessions) {
   const api = express.Router();
@@ -43,7 +44,49 @@ export function TemplatesBucketsAPI(database: Database, sessions: Sessions) {
     }
   });
 
-  // TODO: Create bucket (requires session)
+  api.put("/", express.json(), requireSessionMiddleware(sessions), async (req: RequestWithSession, res) => {
+    let { checkType, completeBatch } = batchCheckType();
+    checkType("name", req.body.name, "string");
+    checkType("cardId", req.body.cardId, "string", { canBeNull: true })
+    checkType("weight", req.body.weight, "number");
+    checkType("standalone", req.body.standalone, "boolean");
+    completeBatch();
+
+    if (req.body.weight <= 0 || req.body.weight > 1)
+      throw new APIError(400, "INVALID_WEIGHT", "Weight must be between 0 (exclusive) and 1 (inclusive).");
+
+    if (!req.body.standalone && req.body.cardId == null)
+      throw new APIError(400, "INVALID_CARD_ID", "Card ID must be provided when not creating a standalone bucket template.");
+
+    if (req.body.standalone && req.body.cardId != null)
+      throw new APIError(400, "INVALID_CARD_ID", "Card ID must not be provided when creating a standalone bucket template.");
+
+    try {
+      if (req.body.cardId !== null)
+        await database.templates.getCard(req.body.cardId);
+
+      let bucket: TemplateBucket;
+      if (req.body.standalone)
+        bucket = await database.templates.createStandaloneBucket(req.body.name, req.body.weight);
+      else
+        bucket = await database.templates.createBucket(req.body.name, req.body.cardId, req.body.weight);
+
+      res.status(200).json({
+        id: bucket.id,
+        name: bucket.name,
+        cardId: bucket.cardId,
+        weight: bucket.weight,
+        standalone: bucket.standalone,
+        prompts: [],
+        createdAt: bucket.createdAt,
+        updatedAt: bucket.updatedAt,
+      });
+    } catch (err) {
+      if (err instanceof ReferenceError)
+        throw new APIError(404, "ENTITY_NOT_FOUND", `Could not find a card template with the id ${req.body.cardId}.`);
+      else throw err;
+    }
+  });
 
   // TODO: Update bucket (requires session)
 
