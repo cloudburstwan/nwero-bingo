e2c4348f825a96ed363d509bcb10e2f0bec52935
Cloudburst <cloudburst@cloudburst.lgbt>
2026-07-11 14:54:42 +0100

    Added API endpoint for updating template cards

--- backend/src/routes/templates/Cards.ts
+++ backend/src/routes/templates/Cards.ts
@@ -80,6 +80,58 @@ export function TemplatesCardsAPI(database: Database, sessions: Sessions) {
   });
 
   // TODO: Update card (requires session)
+  api.patch("/:id", express.json(), requireSessionMiddleware(sessions), async (req: RequestWithSession, res) => {
+    let { checkType, completeBatch } = batchCheckType();
+    checkType("name", req.body.name, "string", { canBeUndefined: true });
+    checkType("description", req.body.description, "string", { canBeUndefined: true, canBeNull: true });
+    checkType("width", req.body.width, "number", { canBeUndefined: true });
+    checkType("height", req.body.height, "number", { canBeUndefined: true });
+    completeBatch();
+
+    try {
+      let card = await database.templates.getCard(req.params.id as string);
+      let oldCard = Object.freeze({...card});
+      let updatedRawData: any = Object.assign(card, req.body);
+
+      card.name = updatedRawData.name;
+      card.description = updatedRawData.description;
+      card.width = updatedRawData.width;
+      card.height = updatedRawData.height;
+
+      await database.templates.updateCard(card);
+
+      await database.addHistory(req.session!.userId, "templates_cards", HistoryAction.UPDATE, card.id, {
+        before: {
+          name: oldCard.name,
+          description: oldCard.description,
+          width: oldCard.width,
+          height: oldCard.height,
+        },
+        after: {
+          name: card.name,
+          description: card.description,
+          width: card.width,
+          height: card.height,
+        }
+      });
+
+      res.status(200).json({
+        id: card.id,
+        name: card.name,
+        description: card.description,
+        width: card.width,
+        height: card.height,
+        createdAt: card.createdAt,
+        updatedAt: card.updatedAt,
+        buckets: await card.getBucketIds(),
+        freeSpaces: await card.getFreeSpaceIds(),
+      });
+    } catch (err) {
+      if (err instanceof ReferenceError)
+        throw new APIError(404, "ENTITY_NOT_FOUND", `Could not find a card with the id ${req.params.id}.`);
+      else throw err;
+    }
+  });
 
   // TODO: Delete card (requires session)
 
